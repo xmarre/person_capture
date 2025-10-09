ddefa82b10f4383514b160881f3b32b128dcace4
xmarre <54859656+xmarre@users.noreply.github.com>
2025-10-09 15:49:06 +0200

    Ensure TensorRT parser DLLs and cache path handling

--- start_person_capture.bat
+++ start_person_capture.bat
@@ -2,25 +2,36 @@
 setlocal
 set "REPO=%~dp0"
 pushd "%REPO%"
+
+rem --- TensorRT absolute path ---
+set "TRT_LIB_DIR=D:\tensorrt\TensorRT-10.13.3.9\lib"
+set "PATH=%TRT_LIB_DIR%;%PATH%"
+
+rem --- activate venv ---
 set "VENV=%REPO%env"
-set "PYTHONW=%VENV%\Scripts\pythonw.exe"
+call "%VENV%\Scripts\activate.bat"
 
-if not exist "%PYTHONW%" (
-  echo Missing venv python: "%PYTHONW%"
-  exit /b 1
-)
-
-rem === add NVIDIA/TensorRT runtime DLL dirs from venv wheels ===
+rem --- CUDA/cuDNN DLLs from wheels (needed before ORT import) ---
 set "PATH=%VENV%\Lib\site-packages\torch\lib;%PATH%"
-set "PATH=%VENV%\Lib\site-packages\tensorrt;%PATH%"
-set "PATH=%VENV%\Lib\site-packages\tensorrt_libs;%PATH%"
-for /R "%VENV%\Lib\site-packages\nvidia" %%F in (nvinfer*.dll nvonnxparser*.dll nvinfer_plugin*.dll) do (
-  set "PATH=%%~dpF;%PATH%"
+for %%D in (cublas cudnn cuda_runtime cuda_nvrtc cufft curand cusolver cusparse) do (
+  if exist "%VENV%\Lib\site-packages\nvidia\%%D\bin" set "PATH=%VENV%\Lib\site-packages\nvidia\%%D\bin;%PATH%"
 )
-for /R "%VENV%\Lib\site-packages\tensorrt_libs" %%F in (nvinfer*.dll nvonnxparser*.dll nvinfer_plugin*.dll) do (
-  set "PATH=%%~dpF;%PATH%"
+if exist "%VENV%\Lib\site-packages\tensorrt" set "PATH=%VENV%\Lib\site-packages\tensorrt;%PATH%"
+if exist "%VENV%\Lib\site-packages\tensorrt_libs" set "PATH=%VENV%\Lib\site-packages\tensorrt_libs;%PATH%"
+
+rem --- quick sanity: required TRT parser DLLs present? ---
+if not exist "%TRT_LIB_DIR%\nvonnxparser.dll" if not exist "%TRT_LIB_DIR%\nvonnxparser_10.dll" (
+  echo ERROR: nvonnxparser*.dll missing in %TRT_LIB_DIR%
+  popd & endlocal & exit /b 2
 )
 
-"%PYTHONW%" -m person_capture.gui_app
+rem --- ORT providers must include TensorRT ---
+python - <<^
+import onnxruntime as ort
+print("ORT providers:", ort.get_available_providers())
+^
+
+rem --- run GUI ---
+python -Xfaulthandler -m person_capture.gui_app
 popd
 endlocal
